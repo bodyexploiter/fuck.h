a9d438da6ec037f8d9955822fbb9bff689032997
bodyexploiter <110399720+bodyexploiter@users.noreply.github.com>
2022-08-01 18:18:29 +0530

    Create fuck.h

---
+++ fuck.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+void fuck(char m[100],char g[100]){
+int i;
+for(i=1;i<=100;i++){
+printf("%s Fucking %s \t",m,g);
+}
+printf("\n%s Fucked %s successfully and she is now pregnant\n\n",m,g);
+}
+
+void fuckher(char g[100]){
+int i;
+for(i=1;i<=100;i++){
+printf("I am Fucking %s \t",g);
+}
+printf("\nI Fucked %s successfully and she is now pregnantn\n\n",g);
+}
+
+void liplock(char m[100],char g[100]){
+  printf("%s is sucking %s's lips\n\n",m,g);
+}
+
+void kiss(char m[100],char g[100]){
+  printf("%s is kissing the neck of %s\n",m,g);
+  printf("%s is kissing the sholders of %s\n",m,g);
+  printf("%s is kissing the upperchest of %s\n",m,g);
+  printf("%s is kissing the armpit of %s\n\n",m,g);
+  }
+
+void undress(char m[100],char g[100],char dressf[100],char dressm[100]){
+  printf("%s is removing the %s top of %s\n",m,dressf,g);
+  printf("%s is pressing the boobs of %s and she is moaning\n",m,g);
+  printf("%s is removing the %s of %s\n",g,dressm,m);
+  printf("%s is kissing the navels of %s\n",m,g);
+  printf("%s is removing the %s legings of %s\n",m,dressf,g);
+  printf("%s is taking off the buttons of pants of %s\n",g,m);
+  printf("%s is removing the pants of %s\n\n",g,m);
+  printf("%s is now in bras and panties\n",g);
+  printf("%s is now on his beniyen and innerwear\n\n",m);  
+}
+
+void strip(char m[100], char g[100]){
+  printf("%s grabbed the pallu of %s's saree and pulled it. Her saree fell down and %s is now in pavaada and blouse with fatty and Perky breasts and a beautiful navel.\n",m,g,g);
+  printf("%s slowly went to %s. He placed his hands on her soft hips and pulled her close to him. He trailed his hand along her soft hips and pulled her close at the back and hugged her. He captured her soft lips with his and began to kiss her lips. He slowly moved his hands on her outer hips to her soft butt. He pressed and squeezed her cotton butt. He squeezed her buttocks with both hands. He ran his lips all over her neck and upper chest. She started humming with pleasure. He squeezed both her breasts well outside the blouse. He unbuttoned and unhooked her blouse. He kissed her armpits and pulled her. He reached up to her navel with a kiss on the top of her bra. He lowered his tongue to her navel. Her breasts rose up with pleasure. He kissed her from her feet to her thighs. He bit the thread of her skirt and untied it. He removed her skirt and changed it. %s is now only in her bras and panties\n\n",m,g,g);
+}
