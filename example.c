cbe1ac76325da7ef30986e9c143e5f15c0b7775b
bodyexploiter <110399720+bodyexploiter@users.noreply.github.com>
2022-08-01 18:19:39 +0530

    Create example.c

---
+++ example.c
@@ -0,0 +1,9 @@
+#include "fuck.h"
+
+int main() {
+  liplock("male","female");
+  kiss("male","female");
+  undress("Adithyan","Teena","Churidar","Shirt");
+  strip("Adithyan","Anupama Parameshwaran");
+  return 0;
+}
